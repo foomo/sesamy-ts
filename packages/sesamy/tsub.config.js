37b4800b1d1a3d230f4057f3f9448e57df4d27ba
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2024-05-14 09:16:07 +0200

    fix: cleanup

--- packages/sesamy/tsub.config.js
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from "tsup";
-export default defineConfig((options) => (Object.assign({ treeshake: true, splitting: true, entry: ['src/**/*.ts', 'src/**/*.tsx'], format: ["esm"], dts: true, minify: true, clean: true, external: ["react"] }, options)));
-//# sourceMappingURL=tsub.config.js.map
